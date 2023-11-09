3f15156272785b20c5a01adcfe642a3d663df8d0
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-11-09 11:18:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -52,8 +52,8 @@ async def give_filter(client, message):
     if k == False:
         await auto_filter(client, message)
 
-@Client.on_message(filters.private & filters.text)
-async def give_filter(client, message):
+@Client.on_message(filters.private & filters.text & filters.incoming)
+async def pm_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
