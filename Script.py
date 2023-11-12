6791e449dbfbdbad2569cf680e31ebb8309a2a92
Shashwat  Kumar Das <121427522+ShashwatKumar01@users.noreply.github.com>
2023-11-12 10:30:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """Hello {},
-Myself <a href=https://t.me/{}>{}</a>,\n\nIts Easy To Use Me Just Add Me To Your Group As Admin and i will Provide Movies/Series There Or \n\nJUST SEND ME THE NAME OF THE MOVIE....🤪"""
+Myself <a href=https://t.me/{}>{}</a>,\n\nJUST SEND ME THE NAME OF THE MOVIE  \n\n If Files are not Available you can try @iMoviestestbot or Request the Movie below....🤪"""
     LZTHMB_TEXT = """Hello {},
 Glad to see you here. It seems that you really love <a href=https://t.me/LazyDeveloperr >LazyDeveloper's</a> work.\n\n<b>Thumbnail extracting</b> feature will be available soon, please join <a href=https://t.me/LazyDeveloper>Dev Channel</a> and stay tuned for next <a href=https://t.me/LazyDeveloper>update</a>.\n\n  🐞 Report Bug here: <a href=http://t.me/LazyDeveloperSupport>LazyDev Support</a>
     """
