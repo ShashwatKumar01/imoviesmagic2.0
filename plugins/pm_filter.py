a782316a58b42fbdcb3c66c28dd7ab42599d65de
Shashwat  Kumar Das <121427522+ShashwatKumar01@users.noreply.github.com>
2023-12-03 10:12:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1461,7 +1461,7 @@ async def auto_filter(client, msg, spoll=False):
 
     btn.insert(0,
         [ 
-	    InlineKeyboardButton(text="⚡ʜᴏᴡ to ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://telegram.me/imoviesmagic'),
+	    InlineKeyboardButton(text="⚡ʜᴏᴡ to ᴅᴏᴡɴʟᴏᴀᴅ⚡", url=DOWNLOAD_TEXT_URL),
         ] 
     )
     if offset != "":
